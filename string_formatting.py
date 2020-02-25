d69e6981db5388cfa88b141cfd71c1d29b6800c5
Dhiv Joseph <djnholdings@gmail.com>
2020-02-25 21:34:00 +1100

    solution

--- string_formatting.py
+++ string_formatting.py
@@ -11,24 +11,18 @@ match the width of the binary value of n.
 
 """
 
-#make a conversion table which converts int to either decimal, octal, hexadecimal (capitalized), binary
-#take in a parameter N as the range of ints to convert
-#create an output function that prints on a single line, the conversion of for each type and then to a new line the next value in the int range
-
 #conversion function to binary, octal, hex
-#1 explore this...can we make this more succinct? Think D.R.Y.
+#1 function converts to int, oct, hex and bin values.
 def convert_all(n):
     for x in range(1,n):
-        print(int(x))
-        print (oct(x).replace("0o", ""))
-        print (hex(x).upper().replace("0X", ""))
-        print (bin(x).replace("0b", ""))
+        print (int(x), oct(x).replace("0o",""), hex(x).upper().replace("0X", ""), bin(x).replace("0b",""))
 
-#2 space padding the output
-test_num = str(1)
-test_num_space = test_num.rjust(4)
-print('This is non-padded: ' + str(test_num))
-print('This is padded by width = binary: ' + str(test_num_space))
+#2 function to space pad values = user input the output
+def print_formatted(n):
+    width = len("{0:b}".format(n))
+    for i in range(1,n+1):
+        print ("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
 
-
-#3 output each result of range n exclusively to that line.
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
