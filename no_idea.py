3f7f9578a2466cb581434a076e86ff20375fbea3
Dhiv Joseph <djnholdings@gmail.com>
2020-03-12 19:14:10 +1100

    added No Idea! HackerRank question description.

--- no_idea.py
+++ no_idea.py
@@ -1,3 +1,17 @@
+'''
+There is an array of n integers. There are also 2 disjoint sets, A and B, each containing m integers. You like all the integers in set A and dislike all the integers in Set B.
+
+Your initial happiness is 0. 
+
+For each i integer in array. of i == A, you add 1 to happiness. 
+
+If i == B, you minus 1 from happiness.
+
+Output your final happiness. Note your happiness can have a negative index range.
+
+'''
+
+
 n, m = input().split()
 array = input().split()
 A = set(input().split())
