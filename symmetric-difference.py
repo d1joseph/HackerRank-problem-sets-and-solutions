e4f93b218e97781c5d28e592fb90be9d8d72c59f
d1joseph <dhiv.joseph@gmail.com>
2021-08-28 21:07:31 +1000

    symmetric difference naive solution

--- symmetric-difference.py
+++ symmetric-difference.py
@@ -6,10 +6,16 @@ The term symmetric difference indicates those values that exist in either M or N
 """
 
 #input and preprocess
-m_len, m = input(), list(map(int, input().split()))
-n_len, n = input(), list(map(int, input().split()))
+m_len, m = input(), set(input().split())
+n_len, n = input(), set(input().split())
 
-    
+# convert sets to list of difference values from m and n
+o, p = list(n.difference(m)), list(m.difference(n))
+q = [int(i) for i in o+p] # convert values to integers
+
+# sort list and print each value
+for i in sorted(q): 
+    print(i)
 
 if __name__ == "__main__":
     pass
